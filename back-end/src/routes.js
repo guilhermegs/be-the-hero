66d50a3fc2dbc720af0329d1cb32d034ccec8784
Guilherme Garcia <guilhermegs@ciandt.com>
2020-03-28 21:38:35 -0300

    Adicionando teste integrado para rota de cadastro de ong

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -14,7 +14,7 @@ routes.post("/ongs", celebrate({
     [Segments.BODY]: Joi.object().keys({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
-        whatsapp: Joi.number().required().min(10000000000).max(99999999999),
+        whatsapp: Joi.string().required().min(10).max(11),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2),
     })
