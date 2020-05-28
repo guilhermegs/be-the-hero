399b2bb47b88253777db61ac676a89cc3e2a00c5
Guilherme Garcia <guilhermegs@ciandt.com>
2020-05-28 15:26:44 -0300

    Ensure OngUseCase returns the same contente returned by LoadOngsRepository

--- clean-back-end/src/domain/usecases/ong-usecase.spec.js
+++ clean-back-end/src/domain/usecases/ong-usecase.spec.js
@@ -11,12 +11,16 @@ class OngUseCase {
 class LoadOngsRepositorySpy {
   constructor () {
     this.calledTimes = 0
+    this.ongs = [{
+      id: 1,
+      name: 'ANY ONG'
+    }]
   }
 
   async load () {
     this.wasCalled = true
     this.calledTimes++
-    return [{}]
+    return this.ongs
   }
 }
 
@@ -27,5 +31,12 @@ describe('Ong UseCase', () => {
     await sut.getOngs()
     expect(loadOngsRepositorySpy.wasCalled).toBe(true)
     expect(loadOngsRepositorySpy.calledTimes).toBe(1)
+  })
+
+  test('Should retun the same content returned by LoadOngsRepository', async () => {
+    const loadOngsRepositorySpy = new LoadOngsRepositorySpy()
+    const sut = new OngUseCase(loadOngsRepositorySpy)
+    const ongs = await sut.getOngs()
+    expect(loadOngsRepositorySpy.ongs).toEqual(ongs)
   })
 })
